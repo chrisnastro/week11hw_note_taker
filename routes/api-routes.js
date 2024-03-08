62f701827f134a4fdcb7d1245c08465b09898f29
chrisnastro <cnastro@icloud.com>
2024-03-07 23:26:40 -0500

    fixed html routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const fs = require ("fs");
+const fs = require("fs");
 
 router.get('/api/notes', async (req, res) => {
-    const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
     res.json(dbJson);
  });
 
  router.post('/api/notes', (req, res) => {
-    const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
     const newComment = {
         title: req.body.title,
         text: req.body.text,
@@ -20,7 +20,7 @@ router.get('/api/notes', async (req, res) => {
  });
 
  router.delete('/api/notes/:id', (req, res) => {
-    let data = fs.readFileSync("db/db.json", "utf8");
+    let data = fs.readFileSync("db/db.json","utf8");
     const dataJSON = JSON.parse(data);
     const newNotes = data.JSON.filter((note) => {
         return note.id !== req.params.id;
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use(html_routes);
+app.use(api_routes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
